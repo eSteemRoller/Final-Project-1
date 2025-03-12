ca3723d7d5b8967ae0302151b93afcfa9ed25677
Jason <jasondoroughonfb@live.com>
2025-03-11 23:40:57 -0700

    Sixth commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
 function openMenu() {
-    document.body.classList.add("menu--open");
-}
-
-function closeMenu() {
-    document.body.classList.remove("menu--open");
-}
+    document.body.classList += " menu--open"
+  }
+  
+  function closeMenu() {
+    document.body.classList.remove('menu--open')
+  }
+  
